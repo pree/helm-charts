649cbe4d8767bf8ef6cb7e45347f9456b994ce76
Pascal Reeb <pascal.reeb@adfinis.com>
2023-12-08 19:02:14 +0100

    fix(gomplate): make it executable

--- .github/readme.sh
+++ .github/readme.sh
@@ -10,6 +10,7 @@ GOMPLATE_VERSION=3.11.6
 
 # install gomplate
 curl --silent --show-error --fail --location --output ./.bin/gomplate https://github.com/hairyhenderson/gomplate/releases/download/v"${GOMPLATE_VERSION}"/gomplate_linux-amd64
+chmod +x ./.bin/gomplate
 
 # validate docs
 gomplate -o README.md -f _templates/readme/README.md.gotmpl -d readme=_templates/readme/README.yaml
