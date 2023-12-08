b7b3a372ddbf582b9c4f3ca509ab86d874ed7879
Pascal Reeb <pascal.reeb@adfinis.com>
2023-12-08 19:00:10 +0100

    fix(gomplate): use correct env var

--- .github/readme.sh
+++ .github/readme.sh
@@ -9,7 +9,7 @@ set -euxo pipefail
 GOMPLATE_VERSION=3.11.6
 
 # install gomplate
-curl --silent --show-error --fail --location --output ./.bin/gomplate https://github.com/hairyhenderson/gomplate/releases/download/v"${HELM_DOCS_VERSION}"/gomplate_linux-amd64
+curl --silent --show-error --fail --location --output ./.bin/gomplate https://github.com/hairyhenderson/gomplate/releases/download/v"${GOMPLATE_VERSION}"/gomplate_linux-amd64
 
 # validate docs
 gomplate -o README.md -f _templates/readme/README.md.gotmpl -d readme=_templates/readme/README.yaml
