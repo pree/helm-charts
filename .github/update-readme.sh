81a03d8078e365009af160cd3e3d1fc0235a9ea3
Pascal Reeb <pascal@reeb.io>
2024-02-29 18:09:22 +0100

    fix(gomplate): remove double v in version

--- .github/update-readme.sh
+++ .github/update-readme.sh
@@ -9,7 +9,7 @@ set -euxo pipefail
 GOMPLATE_VERSION=v3.11.7
 
 # install gomplate
-curl --silent --show-error --fail --location --output ./.bin/gomplate https://github.com/hairyhenderson/gomplate/releases/download/v"${GOMPLATE_VERSION}"/gomplate_linux-amd64
+curl --silent --show-error --fail --location --output ./.bin/gomplate https://github.com/hairyhenderson/gomplate/releases/download/"${GOMPLATE_VERSION}"/gomplate_linux-amd64
 chmod +x ./.bin/gomplate
 
 # validate docs
